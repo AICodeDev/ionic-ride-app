909fb9995bd8e042d5397647765270bdaf7b379e
AICodeDev <145628492+AICodeDev@users.noreply.github.com>
2023-09-20 19:00:08 -0700

    3 files modified

--- pages/about/about.ts
+++ pages/about/about.ts
@@ -11,4 +11,24 @@ export class AboutPage {
 
   }
 
+  angularVersion = 0;
+  ngxMarkdownVersion = '16.0.0';
+
+  markdown = `## Markdown __rulez__!
+---
+
+### Syntax highlight
+\`\`\`typescript
+const language = 'typescript';
+\`\`\`
+
+### Lists
+1. Ordered list
+2. Another bullet point
+   - Unordered list
+   - Another unordered bullet
+
+### Blockquote
+> Blockquote to the max`;
+
 }
