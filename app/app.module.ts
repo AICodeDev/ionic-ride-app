8a81aaaa97917c27764d6448ffb67a0fbb297c2a
AICodeDev <145628492+AICodeDev@users.noreply.github.com>
2023-09-20 19:09:04 -0700

    3 files modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,29 +7,16 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-
+import { MarkdownModule } from 'ngx-markdown';
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
+  declarations: [MyApp, AboutPage, ContactPage, HomePage, TabsPage],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    MarkdownModule.forRoot(),
+    IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  providers: [
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+  entryComponents: [MyApp, AboutPage, ContactPage, HomePage, TabsPage],
+  providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler }],
 })
 export class AppModule {}
